6b55cac65372589a7e8ddd5462c95be1567955bd
shalu <shalushaji999@gmail.com>
2021-09-18 19:12:54 +0530

    updated: moved the common object creation to init () method

--- KataTraining-V1.01/src/test/java/com/kata/training/fizzbuzz/FizzBuzzTest.java
+++ KataTraining-V1.01/src/test/java/com/kata/training/fizzbuzz/FizzBuzzTest.java
@@ -2,26 +2,31 @@ package com.kata.training.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.kata.training.fizzbuzz.FizzBuzz;
 
 @SpringBootTest
 public class FizzBuzzTest {
+	FizzBuzz fizzBuzz;
+	@BeforeEach
+	public void init() {
+		fizzBuzz = new FizzBuzz();
+	}
+	
 	@Test
 	public void fizzPlay() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Fizz", fizzBuzz.play(3));
 	}
 
 	@Test
 	public void fizzPlayWithOtherValues() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Fizz", fizzBuzz.play(6));
 	}
 
 	@Test
 	public void fizzPlayWithNonDivisible() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("5", fizzBuzz.play(5));
-	}
+	}	
 }
